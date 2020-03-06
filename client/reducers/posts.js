06791bd34018e474fe6b02206fcb875c9c9ff581
JamesASchulze <james@jamesschulze>
2020-03-05 22:26:00 -0600

    Video 9: Understanding the Reducer's Job & Dispatching Actions. 20200305

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,11 +1,14 @@
 // A reducer takes in two things:
-
 // 1. the action (info about what happened).
-
 // 2. Copy of current state.
 
+// When a "Dispatch" is fired off, every reducer runs.
+// Logic needs to be built into each reducer to see if
+// it needs to do anything.
+
 function posts(state = [], action) {
-   console.log (state, action);
+   console.log('The post has been updated');
+   console.log(state, action);
    return state;
 }
 
